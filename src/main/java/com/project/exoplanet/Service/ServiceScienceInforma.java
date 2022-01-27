567f7b4dbc73eb519c9d48326a24c4db0e716790
Mario23junior <mariojunior3251@gmail.com>
2022-01-27 01:24:50 -0300

    corrigindo configuracao do  modelMapper

--- src/main/java/com/project/exoplanet/Service/ServiceScienceInforma.java
+++ src/main/java/com/project/exoplanet/Service/ServiceScienceInforma.java
@@ -1,9 +1,15 @@
 package com.project.exoplanet.Service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import com.project.exoplanet.repository.ScientificInforRepository;
 
 @Service
 public class ServiceScienceInforma {
    
+	private ModelMapper mapper;
+	private ScientificInforRepository scienceIndoReposi;
 	
+ 	
 }
