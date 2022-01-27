051215db8d9765c74e81ef055e347694ef84f971
Mario23junior <mariojunior3251@gmail.com>
2022-01-27 00:37:14 -0300

    Configurando repository das entidade de modelo do banco de dados

---
+++ src/main/java/com/project/exoplanet/repository/SubNoturnoRepository.java
@@ -0,0 +1,9 @@
+package com.project.exoplanet.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.exoplanet.model.SubNoturno;
+
+public interface SubNoturnoRepository extends JpaRepository<SubNoturno, Long>{
+
+}
