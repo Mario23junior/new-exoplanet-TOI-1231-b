bd2bb432e37a24182fd8523fbd0d67e057796ca0
Mario23junior <mariojunior3251@gmail.com>
2022-01-27 02:57:25 -0300

    Listando informacoes poor id

--- src/main/java/com/project/exoplanet/controller/ControllerScienceInformation.java
+++ src/main/java/com/project/exoplanet/controller/ControllerScienceInformation.java
@@ -1,12 +1,15 @@
 package com.project.exoplanet.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.exoplanet.Service.ServiceScienceInforma;
-import com.project.exoplanet.model.ScientificInformation;
+import com.project.exoplanet.modelDTO.ScienceInforDTO;
 
 @RestController
 @RequestMapping("/project/api/exoplanet/v1/")
@@ -19,7 +22,12 @@ public class ControllerScienceInformation {
 	}
 	
 	@PostMapping
-	public ScientificInformation save(@RequestBody ScientificInformation scienceInformation ) {
-		return service.save(scienceInformation);
+	public ResponseEntity<ScienceInforDTO> save(@RequestBody ScienceInforDTO scienceInformation ) {
+		return service.saveEntity(scienceInformation);
 	}
+
+    @GetMapping("{id}")
+    public ResponseEntity<ScienceInforDTO> list(@PathVariable Long id) {
+    	return service.listId(id);
+    }
 }
